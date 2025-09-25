09a5c390f5bb0563e45b8388467b4f0128a5d94b
kwill1992 <kwill1992@hotmail.com>
2025-09-24 20:12:14 -0400

    added notes on errors

--- Get to write image.R
+++ Get to write image.R
@@ -3,11 +3,12 @@
 # load hexwall.R function
 source("~/Documents/r-studio-and-git/my_hex_stickers/hexwall/hexwall.R")
 # call hexwall function and assign to "test"
-test <- hexwall("~/Documents/r-studio-and-git/my_hex_stickers/hexwall/samplehex", sticker_row_size = 3, sticker_width = 200)
+# this sometimes get an error -- try adjusting sticker_row_size
+test <- hexwall("~/Documents/r-studio-and-git/my_hex_stickers/my_stickers", sticker_row_size = 5, sticker_width = 200)
 test
 
 
 png("~/Documents/r-studio-and-git/my_hex_stickers/hexwall/samplehex/test123.png")
-hexwall("~/Documents/r-studio-and-git/my_hex_stickers/hexwall/samplehex", sticker_row_size = 3, sticker_width = 200)
+hexwall("~/Documents/r-studio-and-git/my_hex_stickers/hexwall/samplehex", sticker_row_size = 4, sticker_width = 200)
 image_write(test, "~/Documents/r-studio-and-git/my_hex_stickers/hexwall/samplehex/test123.png")
 dev.off()
