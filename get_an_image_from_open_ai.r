3c8844b507972779cc4f86a8a1f81f20177c1367
kwill1992 <kwill1992@hotmail.com>
2025-09-28 16:17:59 -0400

    still trying

--- get_an_image_from_open_ai.r
+++ get_an_image_from_open_ai.r
@@ -1,1 +1,64 @@
 # get an image from Open AI
+
+# starting with this resource
+# https://www.r-bloggers.com/2023/06/how-to-generate-a-hex-sticker-with-openai-and-cropcircles/
+
+# run these if not installed
+# install.packages("openai")
+# install.packages("cropcircles")
+
+library(openai)
+library(cropcircles)
+
+# be sure to get Open Ai api and key working as mentioned in the article
+
+# need to add key to environ
+# Sys.setenv(OPENAI_API_KEY = '<your-key-goes-here>')
+
+# test it
+x <- create_image('a cat with wings staring at a sunset')
+# ok, got url as mentioned in the blog
+
+library(magick)
+image_read(x$data$url)
+# that will plot it in viewer
+
+# couldn't get this to work
+image_write(x$data$url, "dumpsterfire.png")
+
+
+# now to save to files
+y <- image_read(x$data$url)
+image_write(y, "~/Documents/r-studio-and-git/my_hex_stickers/open_ai_generated_images/catwithwingssunset.png")
+
+
+library(cropcircles)
+img_cropped <- hex_crop(
+    images = x$data$url,
+    border_colour = "#107e54",
+    border_size = 24
+)
+
+library(tidyverse)
+library(ggpath)
+library(ggtext)
+library(glue)
+
+ggplot() +
+    geom_from_path(aes(0.5, 0.5, path = img_cropped)) +
+
+    # package name
+    annotate("text", x = 0.5, y = 0.25, label = pkg_name, family = ft, size = 34,
+             fontface = "bold", colour = txt, angle = 22, hjust = 0, lineheight = 0.25) +
+
+    # add github - remove if not wanted
+    annotate("richtext", x=0.46, y = 0.07, family = ft1, size = 10, angle = 15, colour = txt, hjust = 0,
+             label = glue("<span style='font-family:fa-brands; color:{txt}'> </span> {git_name}"),
+             label.color = NA, fill = NA) +
+
+    xlim(0, 1) +
+    ylim(0, 1) +
+    theme_void() +
+    coord_fixed()
+
+ggsave("images/hex-dumpster-fire.png", height = 6, width = 6)
